8e27f6af5f293835d18055944c7f3a0ff64ea344
celestial963 <little.site963@gmail.com>
2025-07-07 21:57:23 +0700

    cleaned redirect pb

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
+// https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/redirects",
-          dest: ".",
-          rename: "_redirects",
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 });
